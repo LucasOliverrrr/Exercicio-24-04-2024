059b234e03b5daef4551e9830f634eae83a77072
LucasOliverrrr <lucasouze@gmail.com>
2024-04-24 09:28:14 -0300

    Exercicio 4

--- App.vue
+++ App.vue
@@ -48,3 +48,21 @@ function increment(){
   <button v-on:click="increment">{{ count }}</button>
 </template>
 -->
+
+
+<!--
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+
+    function onInput(e){
+        text.value = e.target.value
+    }
+</script>
+
+<template>
+  <input v-model="text" placeholder="Type here">
+  <p>{{ text }}</p>
+</template>
+-->
